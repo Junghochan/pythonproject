cd76009cb9c4d8cc191d27c17112749c946b3fb6
Junghochan <81355827+Junghochan@users.noreply.github.com>
2021-04-04 20:18:01 +0900

    Update quiz3.py

--- quiz3.py
+++ quiz3.py
@@ -2,4 +2,4 @@ site = "http://naver.com"
 site1 = site.replace("http://", "")
 site2 = site1[:site1.index(".")]
 pw = site2[:3] + str(len(site2)) + str(site2.count("e")) + "!"
-print(pw)
+print("{0}의 비밀번호는 {1}입니다".format(site, pw))
